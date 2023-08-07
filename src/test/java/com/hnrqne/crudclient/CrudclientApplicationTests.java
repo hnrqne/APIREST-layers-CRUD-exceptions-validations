3af23b58ef0ffc03a79a69c065b95b6a3c289f97
Henrique Sampaio <hnrqne@gmail.com>
2023-08-07 17:47:25 -0300

    Entity client, h2 bd, first api test

---
+++ src/test/java/com/hnrqne/crudclient/CrudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnrqne.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
