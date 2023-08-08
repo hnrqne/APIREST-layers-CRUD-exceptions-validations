0bc295822300d187ebd572ade57b2e7255c5d3dc
Henrique Sampaio <hnrqne@gmail.com>
2023-08-08 16:05:31 -0300

    DELETE Client

--- src/main/java/com/hnrqne/crudclient/services/ClientService.java
+++ src/main/java/com/hnrqne/crudclient/services/ClientService.java
@@ -49,6 +49,10 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		
 		entity.setName(dto.getName());
